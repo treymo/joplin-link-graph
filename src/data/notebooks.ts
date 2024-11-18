09804b9fa329c1e1441b9d241a495ee119ac2ad0
Ben Jaeger <benjaeger99@gmail.com>
2024-11-18 16:39:02 +0000

    feat: finish implementing notebook filter fetching

--- src/data/notebooks.ts
+++ src/data/notebooks.ts
@@ -23,36 +23,75 @@ export async function getFilteredNotebooks(
   shouldFilterChildren: boolean,
   isIncludeFilter: boolean
 ): Promise<Notebook[]> {
-    let filteredNotebooks = getNotebooksByNameAndIDs(filterString)
+    let filteredNotebooks = await getNotebooksByNameAndIDs(filterString)
 
     if (shouldFilterChildren) {
-        filteredNotebooks = getNotebookChildren(filteredNotebooks)
+        filteredNotebooks = await getNotebookChildren(filteredNotebooks)
     }
 
     if (isIncludeFilter) {
-        filteredNotebooks = invertNotebookSelection(filteredNotebooks)
+        filteredNotebooks = await invertNotebookSelection(filteredNotebooks)
     }
 
     return filteredNotebooks
 }
 
-function getNotebooksByNameAndIDs(
+async function getNotebooksByNameAndIDs(
   filterText: string
-): Notebook[] {
-    // TODO: get notebooks
-    return []
+): Promise<Notebook[]> {
+    // TODO: currently only gets by name, not IDs
+
+    let filteredNotebooks: Notebook[] = []
+    const allNotebooks = await getNotebooks()
+
+    for (let text in filterText.split(",")) {
+        let notebooks = allNotebooks
+          .filter(anb => anb.title == text)
+        filteredNotebooks.push(...notebooks)
+    }
+
+    return filteredNotebooks
 }
 
-function getNotebookChildren(
-  filteredNotebooks: Notebook[]
-): Notebook[] {
-    // TODO: get notebook children
-    return []
+async function getNotebookChildren(
+  notebooks: Notebook[]
+): Promise<Notebook[]> {
+    const allNotebooks = await getNotebooks()
+
+    // for every notebook, if
+    //   - it's parent is in the filter list
+    //   - it's not in the filter list itself
+    // are both true, then it's a direct child of the filtered NBs
+    let childrenOfFilteredNBs: Notebook[] =
+      allNotebooks
+        // if the NB already exists in the filtered NB IDs, exclude it
+        .filter(anb => ! notebooks.map(nb => nb.id).includes(anb.id))
+        // if NBs parent exists in the filtered NB IDs, include it
+        .filter(anb => notebooks.map(nb => nb.id).includes(anb.parent_id))
+
+    let lastChildren = childrenOfFilteredNBs
+    while (childrenOfFilteredNBs.length > 0) {
+        notebooks = notebooks.concat(childrenOfFilteredNBs)
+
+        // fetch next set of children not in the filter list
+        childrenOfFilteredNBs =
+          allNotebooks
+            .filter(anb => ! lastChildren
+              .map(nb => nb.id)
+              .includes(anb.id)
+            ).filter(anb => lastChildren
+            .map(nb => nb.id)
+            .includes(anb.parent_id)
+          )
+    }
+
+    return notebooks
 }
 
-function invertNotebookSelection(
-  filteredNotebooks: Notebook[]
-): Notebook[] {
-    // TODO: invert notebook selection
-    return []
+async function invertNotebookSelection(
+  notebooks: Notebook[]
+): Promise<Notebook[]> {
+    const allNotebooks = await getNotebooks()
+
+    return allNotebooks.filter(anb => ! notebooks.map(nb => nb.id).includes(anb.id))
 }
