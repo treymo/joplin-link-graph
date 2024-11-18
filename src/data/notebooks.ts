90ec2dee467f75e27d0cc44e49a07a2626cbb23e
Ben Jaeger <benjaeger99@gmail.com>
2024-11-18 16:28:46 +0000

    wip: add skeleton for new filtering code

--- src/data/notebooks.ts
+++ src/data/notebooks.ts
@@ -17,3 +17,42 @@ export async function getNotebooks(): Promise<Array<Notebook>> {
 
     return allNotebooks;
 }
+
+export async function getFilteredNotebooks(
+  filterString: string,
+  shouldFilterChildren: boolean,
+  isIncludeFilter: boolean
+): Promise<Notebook[]> {
+    let filteredNotebooks = getNotebooksByNameAndIDs(filterString)
+
+    if (shouldFilterChildren) {
+        filteredNotebooks = getNotebookChildren(filteredNotebooks)
+    }
+
+    if (isIncludeFilter) {
+        filteredNotebooks = invertNotebookSelection(filteredNotebooks)
+    }
+
+    return filteredNotebooks
+}
+
+function getNotebooksByNameAndIDs(
+  filterText: string
+): Notebook[] {
+    // TODO: get notebooks
+    return []
+}
+
+function getNotebookChildren(
+  filteredNotebooks: Notebook[]
+): Notebook[] {
+    // TODO: get notebook children
+    return []
+}
+
+function invertNotebookSelection(
+  filteredNotebooks: Notebook[]
+): Notebook[] {
+    // TODO: invert notebook selection
+    return []
+}
