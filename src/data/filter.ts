d5dfe77b6b895ef1a3f866bec5315a53d2f0dcfb
Ben Jaeger <benjaeger99@gmail.com>
2024-11-18 16:45:51 +0000

    feat: implement new note filtering using Notebook array

--- src/data/filter.ts
+++ src/data/filter.ts
@@ -53,3 +53,26 @@ export async function filterNotesByNotebookName(
 
     return filteredNotes;
 }
+
+export function filterNotesByNotebook(
+  noteMap: Map<string, Note>,
+  notebooks: Array<Notebook>
+): Map<string, Note> {
+    if (notebooks.length == 0) {
+        return noteMap
+    }
+
+    let newNoteMap: Map<string, Note> = new Map()
+
+    noteMap.forEach((v, k, m) => {
+        // if note's parent id (the folder it lives in) doesn't match any excluded notebook IDs, include it
+        if (notebooks
+          .filter(nb => nb.id == v.parent_id)
+          .length == 0
+        ) {
+            newNoteMap.set(k, v)
+        }
+    })
+
+    return newNoteMap
+}
