a2b2ba4698cc2b8961d8a7385baae5305ef42dd9
Ben Jaeger <benjaeger99@gmail.com>
2025-01-16 01:00:44 +0000

    feat: add ability to fully abstract filtering into a filterFunc function

--- src/data/filter.ts
+++ src/data/filter.ts
@@ -1,6 +1,39 @@
 import { Note, Notebook } from "./types";
+import { getFilteredNotebooks } from "./notebooks";
 
 // functions to do with filtering go here
+
+/**
+ * Takes all note filter parameters and returns a simplified function that filters a Map of notes
+ *
+ * @param notebookFilterString comma separated string of notebooks to filter by, values can be IDs or names
+ * @param shouldFilterChildren boolean toggle for filtering notes in notebooks that are children of the filters
+ * @param isIncludeFilter boolean toggle to invert selected notebooks
+ */
+export async function getFilterFunction(
+  notebookFilterString: string,
+  shouldFilterChildren: boolean,
+  isIncludeFilter: boolean
+): Promise<(nm: Map<string, Note>) => Map<string, Note>> {
+    let notebooks = await getFilteredNotebooks(
+      notebookFilterString,
+      shouldFilterChildren,
+      isIncludeFilter
+    )
+
+    // TODO: update filter func as more filter options get added
+    let filterFunc =
+      (a: Map<string, Note>) => {
+          if (notebooks.length > 0) {
+              a = filterNotesByNotebook(a, notebooks)
+          }
+
+          return a
+      }
+
+    return filterFunc
+}
+
 
 /**
  * Returns a filtered map of notes by notebook.
