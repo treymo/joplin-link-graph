e565adc413efcaf7fbc2a4a3b170fb252f107247
Trey <dev@treymo.io>
2026-08-31 22:45:53 -0700

    Sync the vendored plugin framework to generator-joplin 3.7.2

--- api/JoplinImaging.d.ts
+++ api/JoplinImaging.d.ts
@@ -1,11 +1,33 @@
+import { ResourceEntity } from '../../database/types';
 import { Rectangle } from './types';
-export interface Implementation {
-    nativeImage: any;
-}
 export interface CreateFromBufferOptions {
     width?: number;
     height?: number;
     scaleFactor?: number;
+}
+export interface CreateFromPdfOptions {
+    /**
+     * The first page to export. Defaults to `1`, the first page in
+     * the document.
+     */
+    minPage?: number;
+    /**
+     * The number of the last page to convert. Defaults to the last page
+     * if not given.
+     *
+     * If `maxPage` is greater than the number of pages in the PDF, all pages
+     * in the PDF will be converted to images.
+     */
+    maxPage?: number;
+    scaleFactor?: number;
+}
+export interface PdfInfo {
+    pageCount: number;
+}
+export interface Implementation {
+    createFromPath: (path: string) => Promise<unknown>;
+    createFromPdf: (path: string, options: CreateFromPdfOptions) => Promise<unknown[]>;
+    getPdfInfo: (path: string) => Promise<PdfInfo>;
 }
 export interface ResizeOptions {
     width?: number;
@@ -24,6 +46,7 @@ export type Handle = string;
  * [View the
  * example](https://github.com/laurent22/joplin/blob/dev/packages/app-cli/tests/support/plugins/imaging/src/index.ts)
  *
+ * <span class="platform-desktop">desktop</span>
  */
 export default class JoplinImaging {
     private implementation_;
@@ -32,11 +55,23 @@ export default class JoplinImaging {
     private createImageHandle;
     private imageByHandle;
     private cacheImage;
+    /**
+     * Creates an image from the provided path. Note that images and PDFs are supported. If you
+     * provide a URL instead of a local path, the file will be downloaded first then converted to an
+     * image.
+     */
     createFromPath(filePath: string): Promise<Handle>;
     createFromResource(resourceId: string): Promise<Handle>;
-    getSize(handle: Handle): Promise<any>;
+    createFromPdfPath(path: string, options?: CreateFromPdfOptions): Promise<Handle[]>;
+    createFromPdfResource(resourceId: string, options?: CreateFromPdfOptions): Promise<Handle[]>;
+    getPdfInfoFromPath(path: string): Promise<PdfInfo>;
+    getPdfInfoFromResource(resourceId: string): Promise<PdfInfo>;
+    getSize(handle: Handle): Promise<{
+        width: number;
+        height: number;
+    }>;
     resize(handle: Handle, options?: ResizeOptions): Promise<string>;
-    crop(handle: Handle, rectange: Rectangle): Promise<string>;
+    crop(handle: Handle, rectangle: Rectangle): Promise<string>;
     toPngFile(handle: Handle, filePath: string): Promise<void>;
     /**
      * Quality is between 0 and 100
@@ -47,15 +82,15 @@ export default class JoplinImaging {
      * Creates a new Joplin resource from the image data. The image will be
      * first converted to a JPEG.
      */
-    toJpgResource(handle: Handle, resourceProps: any, quality?: number): Promise<import("../../database/types").ResourceEntity>;
+    toJpgResource(handle: Handle, resourceProps: Partial<ResourceEntity>, quality?: number): Promise<ResourceEntity>;
     /**
      * Creates a new Joplin resource from the image data. The image will be
      * first converted to a PNG.
      */
-    toPngResource(handle: Handle, resourceProps: any): Promise<import("../../database/types").ResourceEntity>;
+    toPngResource(handle: Handle, resourceProps: Partial<ResourceEntity>): Promise<ResourceEntity>;
     /**
      * Image data is not automatically deleted by Joplin so make sure you call
      * this method on the handle once you are done.
      */
-    free(handle: Handle): Promise<void>;
+    free(handles: Handle[] | Handle): Promise<void>;
 }
